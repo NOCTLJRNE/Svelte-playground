914d5b5d33d69481328646eaf48a386b02c8b544
NOCTLJRNE <baoan1602@gmail.com>
2020-05-15 12:31:36 +0200

    ...forgot to add the file ...

---
+++ src/Scene4_Message.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+  import { blur, draw, fade, fly } from "svelte/transition";
+  import { cubicIn, cubicOut, circIn, circOut } from "svelte/easing";
+  import { createEventDispatcher } from "svelte";
+  export let scene4Start = false;
+</script>
+
+<style>
+  #flex-container {
+    display: flex;
+    /* align-items: center; */
+    justify-content: center;
+  }
+</style>
+
+<div id="flex-container">
+  {#if scene4Start}
+    <p>Scene 4</p>
+  {/if}
+</div>
